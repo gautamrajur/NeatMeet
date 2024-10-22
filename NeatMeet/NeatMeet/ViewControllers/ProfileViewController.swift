315db3d64b9f425636d2146836bb71e4c7bdc9ab
saniya16 <saniyaanklesaria16@gmail.com>
2024-10-22 19:10:08 -0400

    Created profile page files

---
+++ NeatMeet/NeatMeet/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  NeatMeet
+//
+//  Created by Saniya Anklesaria on 10/22/24.
+//
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ NeatMeet/NeatMeet/Views/ProfileView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileView.swift
+//  NeatMeet
+//
+//  Created by Saniya Anklesaria on 10/22/24.
+//
+import UIKit
+
+class ProfileView: UIView {
+
+
+}
