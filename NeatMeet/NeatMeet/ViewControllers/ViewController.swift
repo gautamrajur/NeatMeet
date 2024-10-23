070f2ccd0409c785970c9eb42d83aa23ebbd814d
damyantjain <damyant9999@gmail.com>
2024-10-22 20:48:43 -0400

    Added profile image and search bar

--- NeatMeet/NeatMeet/ViewControllers/ViewController.swift
+++ NeatMeet/NeatMeet/ViewControllers/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let landingView = LandingView()
+
+    override func loadView() {
+        view = landingView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
--- NeatMeet/NeatMeet/Views/LandingView.swift
+++ NeatMeet/NeatMeet/Views/LandingView.swift
@@ -9,12 +9,66 @@ import UIKit
 
 class LandingView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var profileImage: UIImageView!
+    var searchBar: UISearchBar!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+
+        setUpProfileImage()
+        setUpSearchBar()
+
+        initConstraints()
     }
-    */
+
+    private func setUpProfileImage() {
+        profileImage = UIImageView()
+        profileImage.image = UIImage(systemName: "person.fill")
+        profileImage.contentMode = .scaleAspectFill
+        profileImage.clipsToBounds = true
+        profileImage.layer.cornerRadius = 20
+        profileImage.tintColor = .gray
+        profileImage.layer.borderWidth = 1
+        profileImage.layer.borderColor = UIColor.lightGray.cgColor
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(profileImage)
+    }
+
+    private func setUpSearchBar() {
+        searchBar = UISearchBar()
+        searchBar.placeholder = "Search Events"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+
+        searchBar.backgroundImage = UIImage()
+        searchBar.searchTextField.clipsToBounds = true
+
+        addSubview(searchBar)
+    }
+
+    private func initConstraints() {
+        NSLayoutConstraint.activate([
+
+            // profile Image
+            profileImage.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor, constant: 16),
+            profileImage.topAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0),
+            profileImage.widthAnchor.constraint(equalToConstant: 40),
+            profileImage.heightAnchor.constraint(equalToConstant: 40),
+
+            // search Bar
+            searchBar.leadingAnchor.constraint(
+                equalTo: profileImage.trailingAnchor, constant: 12),
+            searchBar.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor, constant: -16),
+            searchBar.centerYAnchor.constraint(
+                equalTo: profileImage.centerYAnchor),
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
