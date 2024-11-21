741d6ab99dd26c5735df62f61fa754aa20842667
damyantjain <damyant9999@gmail.com>
2024-11-21 16:11:44 -0500

    Added Alamofire and API for state and cities

---
+++ NeatMeet/NeatMeet/DataModels/City.swift
@@ -0,0 +1,12 @@
+//
+//  City.swift
+//  NeatMeet
+//
+//  Created by Damyant Jain on 11/21/24.
+//
+
+public struct City : Codable {
+    var name: String
+    var countryCode: String
+    var stateCode: String
+}
